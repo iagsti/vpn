358b9ac106b53bf2d9b3ad70a0189cc5fd202611
Marcelo Schneider <schneider.fei@gmail.com>
2020-10-26 00:23:20 -0300

    Testa criação da Solicitação

--- vpn/core/tests/test_core_models.py
+++ vpn/core/tests/test_core_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.db import models
 
+from vpn.core.tests import mock
 from ..models import Solicitation
 
 
@@ -20,3 +21,15 @@ class CoreModelsTest(TestCase):
             with self.subTest():
                 message = '{} not found'.format(attr)
                 self.assertTrue(hasattr(self.obj, attr), msg=message)
+
+    def test_create_solicitation(self):
+        requester_data = mock.make_user_data(login='77889911')
+        answerable_data = mock.make_user_data(login='22334455')
+        requester = mock.make_user(requester_data)
+        answerable = mock.make_user(answerable_data)
+
+        solicitation_data = mock.make_solicitation_data(
+            requester, answerable)
+        mock.make_solicitation(solicitation_data)
+
+        self.assertTrue(Solicitation.objects.exists())
